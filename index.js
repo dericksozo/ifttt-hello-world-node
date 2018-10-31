5cddd60ac8d88fd916bfa638fce136fa599272e2
Derick Sozo <derick.r.ruiz@gmail.com>
2018-10-31 12:41:29 +0700

    Adding the status endpoint.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// The status endpoint
+app.get('/ifttt/v1/status', (req, res) => {
+  res.status(200).send();
+});
+
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
