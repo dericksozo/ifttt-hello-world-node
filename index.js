c7485b315dfa5abdce45b5a452bfae4e7bd1f196
Derick Sozo <derick.r.ruiz@gmail.com>
2018-10-31 12:55:13 +0700

    Making sure that the status endpoint passes all of the tests.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const API_SERVICE_KEY = process.env.API_SERVICE_KEY;
+
+app.use(bodyParser.json());
 
 // The status endpoint
 app.get('/ifttt/v1/status', (req, res) => {
-  res.status(200).send();
+
+  const IFTTTServiceKey = req.get("IFTTT-Service-Key");
+
+  if (typeof IFTTTServiceKey !== "string" || IFTTTServiceKey === "INVALID") {
+    res.status(401).send();
+  } else {
+    res.status(200).send();  
+  }
+
 });
 
 app.listen(3000, () => {
