e2523df4329b0a994cb19964e973860f0371788e
Derick Sozo <derick.r.ruiz@gmail.com>
2018-10-31 14:10:41 +0700

    Adding an endpoint for the new_thing_created trigger and making sure that it passes all the tests on IFTTT.

--- index.js
+++ index.js
@@ -39,6 +39,62 @@ app.post('/ifttt/v1/test/setup', (req, res) => {
   }
 });
 
+app.post('/ifttt/v1/triggers/new_thing_created', (req, res) => {
+
+  const IFTTTServiceKey = req.get("IFTTT-Service-Key");
+
+  if (typeof IFTTTServiceKey !== "string" || IFTTTServiceKey === "INVALID") {
+    res.status(401).send({
+      errors: [{
+        "message": "Invalid IFTTT Service Key"  
+      }]
+    });
+  };
+
+  const now = new Date(),
+        timestamp = Math.floor(Date.now() / 1000);
+
+  let data = [], // The data will be put in here and returned in the response
+      limit = 50; // IFTTT's default limit
+
+  // Doing these extra checks so that 0 doesn't throw the JavaScript off ;)
+  if (req.body.limit != null && typeof req.body.limit === "number") {
+    limit = req.body.limit;
+  }
+
+  if (limit >= 1) { // Not gonna run through this if limit is 0 to pass the IFTTT endpoint test.
+    for (let i = 0; i < limit; i += 1) {
+      data.push({
+        "created_at": now.toISOString(),
+        "meta": {
+          "id": UTILS.guid(),
+          "timestamp": timestamp
+        }
+      });
+    }
+  }
+
+  res.status(200).send({
+    "data": data
+  });
+
+});
+
+var UTILS = (function () {
+
+  return {
+    guid: function () {
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
+  };
+
+}());
+
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
